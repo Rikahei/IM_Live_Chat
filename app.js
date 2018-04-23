e207f5ecbe5426ba1456dc7f8d673d6306a49369
Alex Li <pokehei2@gmail.com>
2018-04-23 05:24:18 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use('/css', express.static('css'));
+app.use('/img', express.static('img'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log("message: " + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
